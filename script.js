154ada87df2ef6c43b9c512b6bcf57e5d9f7dd45
Jason Carrillo <jason@dotjson.net>
2024-06-29 16:41:27 -0600

    initialize guestbook.html and node server

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let guestbook_info = {
+     name : '',
+     siteURL : '',
+     Message : ''
+}
+
+let guestbook_form = {
+     name : Document.getElementbyID('guestbook_name'),
+     siteURL : Document.getElementbyID('guestbook_url'),
+     message : Document.getElementbyID('guestbook_message')
+}
+
+function collect_form_info(){
+     console.log("test");
+}
+
 // Easter Egg
 const tux = `
          _nnnn_
